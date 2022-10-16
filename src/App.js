a907bd5dd43c46c0321555fb5cc11084358d3de6
niraj.bhattad <asknirajbhattad@gmail.com>
2022-10-16 13:50:07 -0400

    Initial commit to create the UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
 
-export default App;
+import { connect, sendMsg } from "./api";
+
+import Header from './components/Header/Header';
+
+import ChatInput from './components/ChatInput/ChatInput';
+
+import ChatHistory from './components/ChatHistory/ChatHistory';
+
+
+
+class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        chatHistory: []
+      }
+    }
+
+    componentDidMount() {
+      connect((msg) => {
+        console.log("New Message")
+        this.setState(prevState => ({
+          chatHistory: [...this.state.chatHistory, msg]
+        }))
+        console.log(this.state);
+      });
+    }
+  
+    send(event) {
+      if(event.keyCode === 13) {
+        sendMsg(event.target.value);
+        event.target.value = "";
+      }
+    }
+  
+    render() {
+      return (
+        <div className="App">
+          <Header />
+          <ChatHistory chatHistory={this.state.chatHistory} />
+          <ChatInput send={this.send} />
+        </div>
+      );
+    }
+  }
+
+  export default App;
